2c82d18e41aa201288dd66141340afb185111ede
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-04-23 15:20:11 +0400

    Fix invalid directory name

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/kamranahmedse/beetle.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
-cd git-standup
+cd beetle
 make install || { echo >&2 "Installation failed with $?"; exit 1; }
 cd ..
 rm -rf beetle
